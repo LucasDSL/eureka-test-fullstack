8bbed4e8469425721ef6581aaf1eb815651b7370
LucasDSL <lucasdslima2002@gmail.com>
2022-04-09 10:58:52 -0300

    feat-frontend: title component

---
+++ frontend/src/components/Title/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Title.css"
+
+function Title() {
+  return (
+    <div className='titleDescription'>
+        <h1>CEP Info</h1>
+        <h3>Informações sobre CEP</h3>
+    </div>
+  )
+}
+
+export default Title
