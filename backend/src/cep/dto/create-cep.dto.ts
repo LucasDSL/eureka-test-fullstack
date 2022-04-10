43e1788dfeab06a6bf61eba0a66930adbd9d571f
LucasDSL <lucasdslima2002@gmail.com>
2022-04-09 21:36:55 -0300

    fix: impact of entity change

--- backend/src/cep/dto/create-cep.dto.ts
+++ backend/src/cep/dto/create-cep.dto.ts
@@ -4,7 +4,6 @@ export class CreateCepDto {
   city: string;
   stateAcronym: string;
   neighborhood: string;
-  visited = 1;
 
   constructor(viaCepObject) {
     this.cep = viaCepObject.cep.replace('-', '');
